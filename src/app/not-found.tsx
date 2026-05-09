69766cfb5eb9ec8b354ee6f59e4d664851701c67
tanjiyaJuthi <tanjiya098@gmail.com>
2026-05-09 14:57:00 +0600

    update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { motion } from 'framer-motion';
+import { motion } from 'motion/react';
 
 export default function NotFound() {
   return (
